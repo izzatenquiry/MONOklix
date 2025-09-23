61482fa87b104cd99ebff707fb0e66be18b13087
izzatenquiry <99197391+izzatenquiry@users.noreply.github.com>
2025-09-23 16:05:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load .env variables
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -18,14 +17,9 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      host: '0.0.0.0',   // supaya boleh diakses dari luar container
-      port: Number(process.env.PORT) || 8080,
-      strictPort: true,
-    },
-    preview: {
       host: '0.0.0.0',
       port: Number(process.env.PORT) || 8080,
-      allowedHosts: ['app.monoklix.com'], // whitelist domain
+      strictPort: true,
     },
   };
 });
